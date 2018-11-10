90a9e1852168fe932de6fdb911a1238842661ef5
jrob93 <jrobinson72@qub.ac.uk>
2018-11-10 15:53:46 -0600

    run for python build and install

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='coord_transforms',
+      version='0.1dev',
+      description='transform rotating and heliocentric coordinates',
+      author='James Robinson',
+      author_email='jrobinson72@qub.ac.uk',
+      packages=['coord_transforms'],
+     )
